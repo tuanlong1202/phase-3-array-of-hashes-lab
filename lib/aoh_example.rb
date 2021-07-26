ffd190c385da74ac8e255df04cf27704439d791f
tuanlong1202 <tuanlong1202@gmail.com>
2021-07-26 00:30:37 -0400

    Phase 3 array of hashes lab

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -9,17 +9,28 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 def assembled_aoh
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
+  retArray = []
+  retArray << DON_G
+  retArray << JOELLE_VD
+  retArray << PAT_M
+  retArray << KATE_G
+  retArray << BRUCE_G
+  retArray
 end
 
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  retArray = assembled_aoh
 end
 
 def aoh_lookup(aoh, row, key)
+  aoh[row][key]
 end
 
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
+  aoh[row][key] = new_value
+  aoh
 end
